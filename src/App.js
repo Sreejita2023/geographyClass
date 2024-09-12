ea8f2b8be3ed397d676bf24edaae767eef52cab3
Shrayash Shukla <ishrayash@gmail.com>
2024-09-13 02:26:46 +0530

    added to app.js the required code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { useState, useContext } from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importing router-related functions
 import GameApp from "./components/GameApp";
 import { GameProvider } from "./context/GameContext";
 
 function App() {
   return (
-    <>
+    <Router>
       <GameProvider>
         <div className="App">
-          <GameApp />
+          <Routes>
+            <Route path="/" element={<GameApp />} />
+          </Routes>
         </div>
       </GameProvider>
-    </>
+    </Router>
   );
 }
 
